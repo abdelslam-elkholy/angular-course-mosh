b764b836b279d454583f585357bccda34f7bd6ba
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-21 18:26:17 +0300

    update

--- src/app/cours/cours.component.ts
+++ src/app/cours/cours.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CoursesService } from '../courses.service';
 @Component({
   selector: 'app-cours',
   templateUrl: './cours.component.html',
-  styleUrls: ['./cours.component.css']
+  styleUrls: ['./cours.component.css'],
 })
 export class CoursComponent {
+  title: string = 'hey im title';
 
+  courses;
+  getTitle() {
+    return this.title;
+  }
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,5 @@
+export class CoursesService {
+  getCourses() {
+    return ['corse-1', 'corse-2', 'corse-3', 'corse-4'];
+  }
+}
